80088b2b91f72a306f1e747c4268468994a69e62
Kyla Marczewski <marczk@uw.edu>
2019-07-08 15:35:07 -0700

    improved error message template

---
+++ src/app/map/loading/loading.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss']
+})
+export class LoadingComponent implements OnInit {
+  @Input() status : {
+    message: string,
+    error: boolean,
+    info? : string
+  };
+  timeout;
+  showError = false;
+  // error = "error";
+  constructor() { }
+
+  ngOnInit() {
+    this.timeout = setTimeout(() => {
+      if(!this.status.error){
+        this.status.info = "Large requests may take several minutes to load. To reduce load times, try smaller date ranges, less metrics, or more specific queries.";
+        this.showError = true;
+      }
+    }, 1000 * 10); // after 20 seconds tell them requests take a while
+  }
+
+}
